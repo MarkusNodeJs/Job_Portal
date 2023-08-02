de38bb4156d8f88f4c0927b69a3847303d87dc81
MarkusNodeJs <markus.dalton03282023@gmail.com>
2023-08-02 11:55:56 +0800

    server

--- server/server.js
+++ server/server.js
@@ -4,6 +4,11 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import colors from "colors";
 
+// ** IMPORT CONNECTION *
+import connectDB from "./config/db.js";
+
+// ** IMPORT ROUTES *
+
 // ** DOTENV *
 dotenv.config();
 const port = process.env.PORT;
@@ -16,6 +21,9 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// ** DATABASE CONNECTION *
+connectDB();
+
 // ** HTTP GET REQUEST *
 app.get("/", (req, res) => {
   res.send("Wenn Mark Recopelacion");
@@ -23,6 +31,7 @@ app.get("/", (req, res) => {
 
 // ** API ROUTES *
 
+// ** EVENT LISTENER *
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`.bgCyan);
 });
